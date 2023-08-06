ade6c68666420ca16b1bc976259ab6b48e4f729a
RomRon <romron@ukr.net>
2023-08-06 12:59:45 +0300

    что то

--- lesson1/index.php
+++ lesson1/index.php
@@ -1,4 +1,26 @@
 <?php
 
 
-echo '000000000000';
+class Tag {
+	public $name_tag;
+
+
+	function __construct( string $name_tag ) {
+		$this->name_tag = $name_tag;
+	}
+
+
+	public function make_tag() {
+
+		// echo '***********************';
+
+		$str_tag = '<' . $this->name_tag . '>   <' . $this->name_tag . '>';
+		echo $str_tag;
+
+	}
+}
+
+
+
+$tag = new Tag( 'k' );
+$tag->make_tag();
