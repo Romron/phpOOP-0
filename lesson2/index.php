ab6f7eaa1051a1fadea39f4b710c70e86cff3d2d
Rombt <romron@ukr.net>
2023-08-12 11:46:06 +0300

    start

--- lesson2/index.php
+++ lesson2/index.php
@@ -1,135 +1,64 @@
-<?php
+<?php 
 
-interface INode{
-	public function render():string;
+interface IStorage{
+	public function add(string $key, mixed $data):void;
+	public function remove(string $key):void;
+	public function contains(string $key):bool;
+	public function get(string $key): mixed;
 }
-
-class Tag implements INode {
-
-	protected $name;
-	protected $attr = [];
-	protected $str_attrs;
-	protected $tag;
-
-
-	public function __construct( string $name_tag ) {
-		$this->name = $name_tag;
-
-	}
-
-	public function attr( string $name, string $value ) {
-		$this->attr[] = "$name = \"$value\"";
-		return $this;
-	}
-
-	protected function str_attrs() {
-		$this->str_attrs = implode( ' ', $this->attr );
-	}
-
-	public function render():string //здесь собираю весь тег с атрибутамив одиночном теге удаляю закрывающий тег
-	{
-		$this->str_attrs();
-		if ( $this->str_attrs && strlen( $this->str_attrs ) > 0 ) {
-			$this->tag = "<$this->name $this->str_attrs>";
-		} else {
-			$this->tag = "<$this->name>";
-		}
-		return $this->tag;
-	}
-}
-
-class SingleTag extends Tag {
-
-
-	public function render():string {
-		parent::render();
-
-
-
-		return $this->tag;
-	}	
-}
-
-class PairTag extends SingleTag {
-
-	protected $children = [];
-	protected $str_children = '';
-
-	public function appendChild( INode $child ) {
-		$this->children[] = $child;
-
-		return $this;
-	}
-
-	public function render():string {
-		parent::render();
-
-
-		foreach ( $this->children as $value ) {
-			$this->str_children .= $value->render();
-		}
-
-		$position = strpos( $this->tag, '>' );
-		$this->tag = substr_replace( $this->tag, $this->str_children, $position + 1, 0 );
-
-
-		$this->tag = $this->tag . "</$this->name>";
-		return $this->tag;
-	}
-}
-
 
 /**
  * 
  */
-class TextNode implements INode{
-	protected $text;
-
-	public function __construct($text){
-		$this->text = $text;
-
-	}
-
-
-	public function render():string{
-		return $this->text;
+class IStorage extends IStorage{
+	
+	function __construct(argument)
+	{
+		// code...
 	}
 }
 
-$Label = new PairTag( "Label" );
-$hr = new SingleTag( "hr" );
-$text = new TextNode('loremloremloremloremlorem');
 
-$a = ( new PairTag( "a" ) )
-	->attr( 'href', '#' )
-	->attr( 'id', 'qqq' )
-	->attr( 'class', 'my_class' );
-$br = (new SingleTag( "br" ))
-	->attr( 'id', 'qqq' )
-	->attr( 'class', 'my_class' );
+class Animal{
+	public $name;
+	public $health;
+	public $alive;
+	protected $power;
 
-$br_1 = new SingleTag( "br" );
+	public function __construct(string $name, int $health, int $power){
+		$this->name = $name;
+		$this->health = $health;
+		$this->power = $power;
+		$this->alive = true;
+	}
 
-$a->appendChild( $hr );
-$a->appendChild( $Label->appendChild($text) );
+	public function calcDamage(){
+		return $this->power * (mt_rand(100, 300) / 200);
+	}
 
+	public function applyDamage(int $damage){
+		$this->health -= $damage;
 
-
-
-echo htmlspecialchars( $a->render() );
-
-echo $br_1->render();
-echo $hr->render();
-echo $br_1->render();
-echo htmlspecialchars( $br->render() );
-
-echo $text->render();
-
+		if($this->health <= 0){
+			$this->health = 0;
+			$this->alive = false;
+		}
+	}
+}
 
 
+class JSONLogger{
+	protected array $objects = [];
 
+	public function addObject($obj) : void{
+		$this->objects[] = $obj;
+	}
 
+	public function log(string $betweenLogs = ',') : string{
+		$logs = array_map(function($obj){
+			return $obj->jsonSerialize();
+		}, $this->objects);
 
-?>
-
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+		return implode($betweenLogs, $logs);
+	}
+}
